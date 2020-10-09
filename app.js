fe40a6cb1b2568fd411716a1c9914786dabab837
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-10-09 23:18:28 +0300

    complete get all todos

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const  express    = require('express'),
        port       = process.env.PORT || 3000,
        bodyParser = require('body-parser');
 
+var todo = require('./models/todo')
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => res.send('Welcome to todo app'));
+
+// api paths to get all of the todos
+app.get('/todos', (req, res) => {
+    console.log(todo);
+    res.json(todo);
+})
+
+// api path to add more todo
+app.post('/addtodos', (req, res) => {
+    console.log(req.body);
+    res.send('data receive');
+})
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
