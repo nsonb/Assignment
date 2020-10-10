8a997d33709e1cba6aee1962d61f3a42d5ed16fe
Son Bui <33485364+nsonb@users.noreply.github.com>
2020-10-11 00:36:08 +0300

    sorted complete

--- modules/filter.js
+++ modules/filter.js
@@ -6,7 +6,8 @@ filterRouter.use('/', (req, res, next)=>{
     next();
 })
 
-filterRouter.get('/complete/', (req, res) => {
+// route for filtering for complete status
+filterRouter.get('/complete', (req, res) => {
     let filehandler = new FileHandler();
     filehandler.readFile().then(todo => {
         todo = todo.filter(todoNote => todoNote.completed === true);
@@ -14,5 +15,24 @@ filterRouter.get('/complete/', (req, res) => {
     });
 })
 
-//
+// route for getting alphabetically sorted by name
+filterRouter.get('/alphabet', (req, res) => {
+    let filehandler = new FileHandler();
+    filehandler.readFile().then(todo => {
+        var result = todo.sort((a,b) => a.name.localeCompare(b.name));
+        
+        res.json(result);
+    });
+})
+
+// route for getting reverse alphabetically sorted by name
+filterRouter.get('/reverse', (req, res) => {
+    let filehandler = new FileHandler();
+    filehandler.readFile().then(todo => {
+        var result = todo.sort((a,b) => a.name.localeCompare(b.name)*-1);
+        
+        res.json(result);
+    });
+})
+
 module.exports = filterRouter;
